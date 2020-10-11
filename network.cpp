1c46b182ebd1d8a7ce5ac7c8f0adda91aab2c98b
Shao-Wei Chiu <shwchiu@cs.nctu.edu.tw>
2020-10-11 20:25:59 +0800

    Fix compile warning
    The warings of comparison between signed and unsigned integer expressions.

--- network.cpp
+++ network.cpp
@@ -17,13 +17,13 @@ double4d create_d4d(int height,
     srand(time(NULL));
     double4d new_fmap;
     new_fmap.resize(numbers);
-    for (int n = 0; n < new_fmap.size(); n++) {
+    for (unsigned n = 0; n < new_fmap.size(); n++) {
         new_fmap[n].resize(channels);
-        for (int c = 0; c < new_fmap[n].size(); c++) {
+        for (unsigned c = 0; c < new_fmap[n].size(); c++) {
             new_fmap[n][c].resize(width);
-            for (int i = 0; i < new_fmap[n][c].size(); i++) {
+            for (unsigned i = 0; i < new_fmap[n][c].size(); i++) {
                 new_fmap[n][c][i].resize(height);
-                for (int j = 0; j < new_fmap[n][c][i].size(); j++)
+                for (unsigned j = 0; j < new_fmap[n][c][i].size(); j++)
                     new_fmap[n][c][i][j] =
                         random ? (double) (rand() % 256) : 0.0;
             }
@@ -34,7 +34,7 @@ double4d create_d4d(int height,
 
 void init_filters(map<string, double4d> *filters, string *f_names, int2d sizes)
 {
-    for (int i = 0; i < sizes.size(); i++) {
+    for (unsigned i = 0; i < sizes.size(); i++) {
         double4d tmp = create_d4d(sizes[i][0], sizes[i][1], sizes[i][2],
                                   sizes[i][3], true);
         filters->insert(pair<string, double4d>(f_names[i], tmp));
@@ -46,7 +46,7 @@ void init_biases(map<string, vector<double> > *biases,
                  int2d sizes)
 {
     srand(time(NULL));
-    for (int i = 0; i < sizes.size(); i++) {
+    for (unsigned i = 0; i < sizes.size(); i++) {
         vector<double> tmp(sizes[i][3]);
         for (int j = 0; j < sizes[i][3]; j++)
             tmp[j] = rand() % 10;
@@ -68,10 +68,10 @@ double4d conv_layer(double4d input,
                                        input[0][0][0].size() + 2 * padding_size,
                                        input[0].size(), input.size(), false);
 
-    for (int n = 0; n < padded_input.size(); n++) {
-        for (int c = 0; c < padded_input[n].size(); c++) {
-            for (int i = 1; i < padded_input[n][c].size() - 1; i++) {
-                for (int j = 1; j < padded_input[n][c][i].size() - 1; j++)
+    for (unsigned n = 0; n < padded_input.size(); n++) {
+        for (unsigned c = 0; c < padded_input[n].size(); c++) {
+            for (unsigned i = 1; i < padded_input[n][c].size() - 1; i++) {
+                for (unsigned j = 1; j < padded_input[n][c][i].size() - 1; j++)
                     padded_input[n][c][i][j] =
                         input[n][c][i - padding_size][j - padding_size];
             }
@@ -93,16 +93,16 @@ double4d conv_layer(double4d input,
                    filter.size() +
                bias.size());
     int macs = 0;
-    for (int n = 0; n < output.size(); n++) {
-        for (int m = 0; m < output[0].size(); m++) {
-            for (int x = 0; x < output[0][0].size(); x++) {
-                for (int y = 0; y < output[0][0][0].size(); y++) {
+    for (unsigned n = 0; n < output.size(); n++) {
+        for (unsigned m = 0; m < output[0].size(); m++) {
+            for (unsigned x = 0; x < output[0][0].size(); x++) {
+                for (unsigned y = 0; y < output[0][0][0].size(); y++) {
                     output[n][m][x][y] = bias[m];
-                    for (int k = 0; k < filter[m].size();
+                    for (unsigned k = 0; k < filter[m].size();
                          k++) {  // filter_channel
-                        for (int i = 0; i < filter[m][k].size();
+                        for (unsigned i = 0; i < filter[m][k].size();
                              i++) {  // filter_height
-                            for (int j = 0; j < filter[m][k][i].size();
+                            for (unsigned j = 0; j < filter[m][k][i].size();
                                  j++) {  // filter_width
                                 output[n][m][x][y] +=
                                     (padded_input[n][k][U * x + i][U * y + j] *
@@ -168,13 +168,13 @@ double2d flatten_2d(double4d input)
 {
     double2d output;
     output.resize(input.size());  // batch size
-    for (int i = 0; i < output.size(); i++)
+    for (unsigned i = 0; i < output.size(); i++)
         output[i].resize(input[0].size() * input[0][0].size() *
                          input[0][0][0].size());
-    for (int n = 0; n < input.size(); n++) {
-        for (int m = 0; m < input[0].size(); m++) {
-            for (int x = 0; x < input[0][0].size(); x++) {
-                for (int y = 0; y < input[0][0][0].size(); y++) {
+    for (unsigned n = 0; n < input.size(); n++) {
+        for (unsigned m = 0; m < input[0].size(); m++) {
+            for (unsigned x = 0; x < input[0][0].size(); x++) {
+                for (unsigned y = 0; y < input[0][0][0].size(); y++) {
                     output[n][m * input[0][0].size() +
                               x * input[0][0][0].size() + y] =
                         input[n][m][x][y];
@@ -202,27 +202,27 @@ double2d fc_layer(double2d input, string layer_name, int size)
     srand(time(NULL));
     bias.resize(input.size());  // batch size
     output.resize(input.size());
-    for (int i = 0; i < bias.size(); i++) {
+    for (unsigned i = 0; i < bias.size(); i++) {
         bias[i].resize(size);
         output[i].resize(size);
-        for (int j = 0; j < bias[i].size(); j++) {
+        for (unsigned j = 0; j < bias[i].size(); j++) {
             bias[i][j] = (double) (rand() % 10);
             output[i][j] = 0.0;
         }
     }
     params.resize(input[0].size());
-    for (int i = 0; i < params.size(); i++) {
+    for (unsigned i = 0; i < params.size(); i++) {
         params[i].resize(size);
-        for (int j = 0; j < params[i].size(); j++)
+        for (unsigned j = 0; j < params[i].size(); j++)
             params[i][j] = (double) (rand() % 10);
     }
     printf("%s\t(1, 1, %lu, %lu)\t\t\t%lu\t\t", layer_name.c_str(), output.size(),
            output[0].size(), params.size() * params[0].size() + bias[0].size());
     int macs = 0;
-    for (int n = 0; n < output.size(); n++) {
-        for (int k = 0; k < output[n].size(); k++) {
+    for (unsigned n = 0; n < output.size(); n++) {
+        for (unsigned k = 0; k < output[n].size(); k++) {
             output[n][k] = bias[n][k];
-            for (int i = 0; i < params[k].size(); i++) {
+            for (unsigned i = 0; i < params[k].size(); i++) {
                 output[n][k] += input[n][i] * params[i][k];
                 macs++;
             }
